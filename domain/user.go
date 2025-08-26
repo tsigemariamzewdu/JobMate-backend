5ac9ce38841c61b744b7617bd2151e6dfe1e704e
Tsigemariam Zewdu <tsigemariamzewdu20@gmail.com>
2025-08-26 11:01:05 +0300

    define user struct

--- domain/user.go
+++ domain/user.go
@@ -1,1 +1,46 @@
-package domain
+package models
+
+import "time"
+
+// PreferredLanguage enum
+type PreferredLanguage string
+
+const (
+	LanguageAmharic PreferredLanguage = "am"
+	LanguageEnglish PreferredLanguage = "en"
+)
+
+// EducationLevel enum
+type EducationLevel string
+
+const (
+	EducationHighSchool EducationLevel = "high_school"
+	EducationDiploma    EducationLevel = "diploma"
+	EducationBachelor   EducationLevel = "bachelor"
+	EducationMaster     EducationLevel = "master"
+	EducationPhD        EducationLevel = "phd"
+	EducationOther      EducationLevel = "other"
+)
+
+type User struct {
+	UserID            string
+	// OauthID           int
+	Phone             *string
+	IsVerified        bool
+	Email             *string
+	FirstName         *string
+	LastName          *string
+	PreferredLanguage *PreferredLanguage
+	EducationLevel    *EducationLevel
+	FieldOfStudy      *string
+	PasswordHash      *string
+	YearsExperience   *int
+	CareerInterests   *string
+	CareerGoals       *string
+	ProfilePicture    *string
+	RefreshToken   	  *string
+	AccessToken       *string
+	// LastActiveAt      time.Time
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+}
